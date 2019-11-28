ae54e1d645722c3085af05d926989208c1316754
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2019-11-28 14:10:42 +0100

    chore: remove user me from MainContext

--- src/containers/MainContext/MainContext.tsx
+++ src/containers/MainContext/MainContext.tsx
@@ -1,11 +1,9 @@
 import React, { useState, createContext, useMemo, useContext } from 'react'
-import { FeedItem, User } from 'src/api'
+import { FeedItem } from 'src/api'
 
 interface MainContextValue {
   feedItem: FeedItem | null;
   onChangeFeedItem: (value: MainContextValue['feedItem']) => void;
-  me: User;
-  setMe: (value: MainContextValue['me']) => void;
 }
 
 const MainContext = createContext<MainContextValue>({} as MainContextValue)
@@ -16,20 +14,16 @@ export function useMainContext() {
 
 interface ProviderProps {
   children: React.ReactChild;
-  me: MainContextValue['me'];
 }
 
 export function Provider(props: ProviderProps) {
-  const { me: meProp, children } = props
+  const { children } = props
   const [feedItem, onChangeFeedItem] = useState<MainContextValue['feedItem']>(null)
-  const [me, setMe] = useState<MainContextValue['me']>(meProp)
 
   const contextValue = useMemo(() => ({
     feedItem,
     onChangeFeedItem,
-    me,
-    setMe,
-  }), [feedItem, me])
+  }), [feedItem])
 
   return (
     <MainContext.Provider value={contextValue}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Reset } from 'styled-reset'
-import NextApp, { AppContext, AppInitialProps } from 'next/app'
+import NextApp from 'next/app'
 import Head from 'next/head'
 import { ReactQueryConfigProvider } from 'react-query'
 import { ThemeProvider } from '@material-ui/core/styles'
@@ -10,7 +10,6 @@ import { Nav } from 'src/containers/Nav'
 import { Layout } from 'src/components/Layout'
 import { MapProvider } from 'src/components/Map'
 import { Provider as MainContextProvider } from 'src/containers/MainContext'
-import { api, User } from 'src/api'
 
 export default class App extends NextApp {
   // Only uncomment this method if you have blocking data requirements for
@@ -18,23 +17,18 @@ export default class App extends NextApp {
   // perform automatic static optimization, causing every page in your app to
   // be server-side rendered.
 
-  static async getInitialProps(appContext: AppContext): Promise<AppInitialProps & { me: User; }> {
-    // calls page's `getInitialProps` and fills `appProps.pageProps`
-    const appProps = await NextApp.getInitialProps(appContext)
+  // static async getInitialProps(appContext: AppContext): Promise<AppInitialProps> {
+  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
+  //   const appProps = await NextApp.getInitialProps(appContext)
 
-    const meResponse = await api.ssr(appContext.ctx).request({
-      routeName: 'GET users/me',
-    })
-
-    return {
-      me: meResponse.data.user,
-      ...appProps,
-    }
-  }
+  //   return {
+  //     ...appProps,
+  //   }
+  // }
 
   render() {
     // @ts-ignore
-    const { Component, pageProps, me } = this.props
+    const { Component, pageProps } = this.props
 
     return (
       <>
@@ -46,7 +40,7 @@ export default class App extends NextApp {
         <Reset />
         <ThemeProvider theme={theme}>
           <ReactQueryConfigProvider config={queryConfig}>
-            <MainContextProvider me={me}>
+            <MainContextProvider>
               <MapProvider>
                 <Layout>
                   <>
