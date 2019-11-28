2af0c8b35eb8cca72938f6ad82f634fff4c8e068
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2019-11-28 17:32:18 +0100

    clean

--- src/network/api/services/login.ts
+++ src/network/api/services/login.ts
@@ -13,26 +13,3 @@ export async function createAnonymousUser(ctx?: NextPageContext): Promise<string
 
   return anonymousToken
 }
-
-// export async function autoLoginOnStart() {
-//   try {
-//     const localToken = getTokenFromCookies()
-
-//     console.log('localToken', localToken)
-
-//     if (localToken) {
-//       try {
-//         await api.request({
-//           routeName: 'GET users/me',
-//         })
-//       } catch (e) {
-//         await loginAnonymousUser()
-//       }
-//     } else {
-//       await loginAnonymousUser()
-//     }
-//   } catch (e) {
-//     console.error(e)
-//     throw new Error('LOGIN_FAILED')
-//   }
-// }
