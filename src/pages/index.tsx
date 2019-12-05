612fc634ea67edb35e6d3c06dfd77555ed926159
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2019-12-05 18:29:15 +0100

    fix: temp redirection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import { StatelessPage } from 'src/types'
 
 interface Props {}
 
 const Home: StatelessPage<Props> = () => {
+  useEffect(() => {
+    // temp hard redirection
+    window.location.href = '/actions'
+  }, [])
+
   return (
     <>
       <Head>
