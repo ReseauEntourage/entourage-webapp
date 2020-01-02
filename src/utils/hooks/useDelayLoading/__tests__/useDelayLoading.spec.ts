dc174ab5da80b82cbdbaae09b5a1ada27f0da7ce
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2020-01-02 11:34:01 +0100

    chore(tests): useDelayLoading() unit tests

---
+++ src/utils/hooks/useDelayLoading/__tests__/useDelayLoading.spec.ts
@@ -0,0 +1,48 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useDelayLoading } from '../useDelayLoading'
+
+function timeout(delay: number) {
+  return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
+const options = {
+  minDelayEndLoading: 2000,
+  minDelayStartLoading: 500,
+}
+
+describe('useDelayLoading', () => {
+  it('should doesn\'t change loading state before 500ms', async () => {
+    const { result } = renderHook(() => useDelayLoading(false, options))
+
+    await act(async () => {
+      result.current[1](true)
+      expect(result.current[0]).toBe(false)
+
+      await timeout(200)
+      expect(result.current[0]).toBe(false)
+      result.current[1](false)
+
+      await timeout(1000)
+      expect(result.current[0]).toBe(false)
+    })
+  })
+
+  it('should change loading state aftet 500ms', async () => {
+    const { result } = renderHook(() => useDelayLoading(false, options))
+
+    await act(async () => {
+      result.current[1](true)
+      expect(result.current[0]).toBe(false)
+
+      await timeout(1000)
+      expect(result.current[0]).toBe(true)
+
+      result.current[1](false)
+      await timeout(1000)
+      expect(result.current[0]).toBe(true)
+
+      await timeout(1500)
+      expect(result.current[0]).toBe(false)
+    })
+  })
+})
