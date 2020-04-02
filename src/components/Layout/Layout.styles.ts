b28f5e4254c20d4689820e4b471979aaf25306ed
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2020-04-02 18:12:47 +0200

    chore - transform Layout from class to function

---
+++ src/components/Layout/Layout.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  a {
+    text-decoration: none;
+  }
+`
+
+export const Nav = styled.div`
+  flex: 0;
+`
+
+export const Page = styled.div`
+  flex: 1;
+  overflow: hidden;
+`
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,40 +1,18 @@
 import { Typography } from '@material-ui/core'
 import React from 'react'
-import styled from 'styled-components'
 import { variants } from 'src/styles'
+import { Main, Page, Nav } from './Layout.styles'
 
-const Main = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-
-  a {
-    text-decoration: none;
-  }
-`
-
-const Nav = styled.div`
-  flex: 0;
-`
-
-const Page = styled.div`
-  flex: 1;
-  overflow: hidden;
-`
-
-export class Layout extends React.PureComponent<{children: JSX.Element;}> {
-  static Nav = Nav
-
-  static Page = Page
-
-  render() {
-    const { children } = this.props
-    return (
-      <Typography component="div" variant={variants.bodyRegular}>
-        <Main>
-          {children}
-        </Main>
-      </Typography>
-    )
-  }
+export function Layout(props: {children: JSX.Element;}) {
+  const { children } = props
+  return (
+    <Typography component="div" variant={variants.bodyRegular}>
+      <Main>
+        {children}
+      </Main>
+    </Typography>
+  )
 }
+
+Layout.Nav = Nav
+Layout.Page = Page
