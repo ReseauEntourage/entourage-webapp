3799ff825b1d2236b7c955ae901084f39e90a567
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2020-04-23 19:52:04 +0200

    feat: message pagination

--- src/core/store/queries/useQueryEntourageChatMessages.ts
+++ src/core/store/queries/useQueryEntourageChatMessages.ts
@@ -1,10 +1,25 @@
+import uniqBy from 'lodash/uniqBy'
+import { useCallback, useState, useEffect } from 'react'
 import { useQuery } from 'react-query'
-import { api } from 'src/core/api'
+import { api, schema } from 'src/core/api'
 import { queryKeys } from 'src/core/store'
 import { assertIsDefined } from 'src/utils/misc'
+import { AnyCantFix } from 'src/utils/types'
+
+type Messages = typeof schema['/entourages/:entourageId/chat_messages GET']['response']['chatMessages']
 
 export function useQueryEntourageChatMessages(entourageId: number | null) {
-  return useQuery(!entourageId ? null : [queryKeys.chatMessage(entourageId), { entourageId }], () => {
+  const [messages, setMessages] = useState<Messages>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const queryKey = !entourageId
+    ? null
+    : [
+      queryKeys.chatMessage(entourageId),
+      { entourageId, before: undefined },
+    ] as [string, { before?: string; entourageId: number | null; }]
+
+  const queryRes = useQuery(queryKey, (params) => {
     assertIsDefined(entourageId, 'useQueryEntourageChatMessages required entourageId')
 
     return api.request({
@@ -12,6 +27,49 @@ export function useQueryEntourageChatMessages(entourageId: number | null) {
       pathParams: {
         entourageId,
       },
+      params: {
+        before: params.before,
+      },
     })
+  }, {
+    paginated: true,
+    getCanFetchMore: (lastPage: AnyCantFix) => {
+      return lastPage.data.chatMessages.length === 25
+    },
+    staleTime: 0,
   })
+
+  const { data: pages } = queryRes
+
+  const fetchMore = useCallback(() => {
+    const lastMessage = messages[messages.length - 1]
+
+    queryRes.fetchMore({
+      before: lastMessage.createdAt,
+      entourageId: null,
+    })
+  }, [messages, queryRes])
+
+  useEffect(() => {
+    const data = !pages
+      ? []
+      : pages
+        .map((res) => res.data.chatMessages)
+        .reduce((pageA, pageB) => [...pageA, ...pageB], [])
+
+    setMessages((prevMessages) => {
+      const uniqMessages = uniqBy([...prevMessages, ...data], (message) => message.id)
+      uniqMessages.sort((a, b) => b.id - a.id)
+      return uniqMessages
+    })
+
+    setIsLoading(false)
+  }, [pages])
+
+  return {
+    ...queryRes,
+    isLoading,
+    data: messages,
+    fetchMore,
+  }
 }
--- src/utils/hooks/useOnScroll.ts
+++ src/utils/hooks/useOnScroll.ts
@@ -1,38 +1,81 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
-const OFFSET_TO_FETCH = 50
+const OFFSET_TO_FETCH = 150
 
 const enableFetchRef = {
   current: true,
 }
 
-export function useOnScroll(fetch: () => Promise<unknown>) {
-  // const enableFetchRef = useRef(true)
+interface UseOnScrollParams {
+  onScrollBottomEnd?: () => void | Promise<unknown>;
+  onScrollTopEnd?: () => void | Promise<unknown>;
+}
+
+export function useOnScroll(params: UseOnScrollParams = {}) {
+  const [isAtTop, setIsAtTop] = useState(false)
+  const [isAtBottom, setIsAtBottom] = useState(false)
+
+  const { onScrollBottomEnd, onScrollTopEnd } = params
 
   const onScroll = useCallback(async (event: React.UIEvent<HTMLElement>) => {
     const { scrollHeight, offsetHeight, scrollTop } = event.currentTarget
     const scrollExist = scrollHeight > offsetHeight
+    // console.log({ scrollHeight, offsetHeight, scrollTop })
+
+    // console.log('scrollExist', scrollExist)
 
     if (!scrollExist) {
       return
     }
 
     const scrollIsAtBottom = (offsetHeight + scrollTop) > scrollHeight - OFFSET_TO_FETCH
+    const scrollIsAtTop = (scrollTop - OFFSET_TO_FETCH) < 0
 
-    if (scrollIsAtBottom) {
+    if (scrollIsAtBottom && !isAtBottom) {
+      setIsAtBottom(true)
+    } else if (!scrollIsAtBottom && isAtBottom) {
+      setIsAtBottom(false)
+    }
+
+    if (scrollIsAtTop && !isAtTop) {
+      setIsAtTop(true)
+    } else if (!scrollIsAtTop && isAtTop) {
+      setIsAtTop(false)
+    }
+
+    if (scrollIsAtBottom && onScrollBottomEnd) {
       if (enableFetchRef.current) {
         enableFetchRef.current = false
         try {
-          await fetch()
+          await onScrollBottomEnd()
           enableFetchRef.current = true
         } catch (e) {
           // do nothing
         }
       }
     }
-  }, [fetch])
+
+    if (scrollIsAtTop && onScrollTopEnd) {
+      if (enableFetchRef.current) {
+        enableFetchRef.current = false
+        try {
+          await onScrollTopEnd()
+          enableFetchRef.current = true
+        } catch (e) {
+          // do nothing
+        }
+      }
+    }
+  }, [
+    onScrollBottomEnd,
+    onScrollTopEnd,
+    isAtTop,
+    isAtBottom,
+  ])
 
   return {
     onScroll,
+    isAtBottom,
+    isAtTop,
   }
 }
