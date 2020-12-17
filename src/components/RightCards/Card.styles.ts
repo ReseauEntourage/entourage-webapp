3bc0083d589d0bd8a56be3da111b6b7418c08728
Guillaume Jasmin <3513444+GuillaumeJasmin@users.noreply.github.com>
2020-12-18 00:07:23 +0100

    [EN-3242] :lipstick: fix - color description link

--- src/components/RightCards/Card.styles.ts
+++ src/components/RightCards/Card.styles.ts
@@ -1,9 +1,12 @@
 import Typography from '@material-ui/core/Typography'
 import styled from 'styled-components'
-import { variants } from 'src/styles'
+import { variants, colors } from 'src/styles'
 
 export const Description = styled(Typography).attrs(() => ({
   variant: variants.bodyRegular,
 }))`
   white-space: pre-line;
+  a {
+    color: ${colors.main.primary};
+  }
 `
