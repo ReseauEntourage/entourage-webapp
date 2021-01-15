7c158f4dcd5915ab1c0bdfe79f8b12705b127611
Emile Bex <emile.bex@gmail.com>
2021-01-15 15:46:34 +0100

    [EN-3437] fix(server): get port from environment variables

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const enforce = require('express-sslify');
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -17,7 +19,7 @@ app.prepare()
       return handle(req, res)
     })
 
-    server.listen(3000, (err) => {
+    server.listen(port, (err) => {
       if (err) throw err
       console.log('> Ready on http://localhost:3000')
     })
