b09ae1d5981691032b67aab3f242f7b8e57a81cb
Loïc Le Goff <dev.legoff@gmail.com>
2021-03-17 09:02:35 +0100

    [EN-3269] 🐛 bug(Feed): fix usage of selector

--- src/containers/MapContainer/LeftLists/FeedFiltersSelector/SectionFilter/HeadFilter.tsx
+++ src/containers/MapContainer/LeftLists/FeedFiltersSelector/SectionFilter/HeadFilter.tsx
@@ -1,17 +1,18 @@
 import Switch from '@material-ui/core/Switch'
 import React, { useCallback } from 'react'
-import { useStore, useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectIsActiveFilter, feedActions } from 'src/core/useCases/feed'
+import { AppState } from 'src/core/useCases/reducers'
 import { texts } from 'src/i18n'
 import { LineFilterProps } from './LineFilter'
 import * as S from './styles'
 
 export function HeadFilter(props: Omit<LineFilterProps, 'category'>) {
   const { index, type } = props
-  const store = useStore()
   const dispatch = useDispatch()
 
-  const checked = selectIsActiveFilter(store.getState(), type)
+  const checked = useSelector<AppState, boolean>((state) => selectIsActiveFilter(state, type))
+
   const onChange = useCallback(() => {
     dispatch(feedActions.toggleFeedFilter({ type }))
   },
--- src/containers/MapContainer/LeftLists/FeedFiltersSelector/SectionFilter/LineFilter.tsx
+++ src/containers/MapContainer/LeftLists/FeedFiltersSelector/SectionFilter/LineFilter.tsx
@@ -1,7 +1,8 @@
 import Switch from '@material-ui/core/Switch'
 import React, { useCallback } from 'react'
-import { useStore, useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectIsActiveFilter, feedActions } from 'src/core/useCases/feed'
+import { AppState } from 'src/core/useCases/reducers'
 import { texts } from 'src/i18n'
 import { FilterEntourageType, FilterFeedCategory } from 'src/utils/types'
 import { mapCategoriesToIcons } from './helper'
@@ -16,14 +17,13 @@ export interface LineFilterProps {
 export function LineFilter(props: LineFilterProps) {
   const { index, type, category } = props
   const CategoryIcon = mapCategoriesToIcons[category]
-  const store = useStore()
+
   const dispatch = useDispatch()
 
-  const checked = selectIsActiveFilter(store.getState(), type, category)
+  const checked = useSelector<AppState, boolean>((state) => selectIsActiveFilter(state, type, category))
   const onChange = useCallback(() => {
     dispatch(feedActions.toggleFeedFilter({ type, category }))
-  },
-  [dispatch, type, category])
+  }, [dispatch, type, category])
 
   const label = texts.content.map.filters[type].category[category]
 
