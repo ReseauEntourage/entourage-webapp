124f57874be06519dd6aaa377c98ae61b16852d0
Guillaume Jasmin <3513444+GuillaumeJasmin@users.noreply.github.com>
2022-05-06 23:41:41 +0200

    chore(dependencies): upgrade typescript 4.3 (#132)

---
+++ src/utils/types/StrictUnion.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type UnionKeys<T> = T extends any ? keyof T : never;
+
+type StrictUnionHelper<T, TAll> = T extends any
+  ? T & Partial<Record<Exclude<UnionKeys<TAll>, keyof T>, never>>
+  : never;
+
+export type StrictUnion<T> = StrictUnionHelper<T, T>;
